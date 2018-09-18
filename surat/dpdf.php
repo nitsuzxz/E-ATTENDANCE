319a846b341727444b7051abb6edbd6952497bb6
successpta <40647688+successpta@users.noreply.github.com>
2018-09-18 18:04:30 +0800

    -

--- surat/dpdf.php
+++ surat/dpdf.php
@@ -11,14 +11,13 @@ $dompdf = new Dompdf();
 
 // Load content from html file
 $html = file_get_contents("surat.php");
-$dompdf->loadHtmlfile($html);
+$dompdf->loadHtml($html);
 // (Optional) Setup the paper size and orientation
 $dompdf->setPaper('A4', 'potrait');
 
 
 // Render the HTML as PDF
 $dompdf->render();
-
 // Output the generated PDF (1 = download and 0 = preview)
 $dompdf->stream('kehadiran'.'.php');
 
